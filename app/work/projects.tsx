220d81830d4fb7787231708d27f2a2399fd1ce56
Akshey D <aksheyd@umich.edu>
2024-12-02 13:26:44 -0500

    fixed nav, added project updates

---
+++ app/work/projects.tsx
@@ -0,0 +1,25 @@
+let projects = [
+    {
+        name: "Gemini AI ASL Translator",
+        bio: "An American Sign Language (ASL) Translator utilizing Google's Gemini API",
+        desc: "Collaborated with 2 team members to create a React and Flask web app to allow users to record videos signing and translate American Sign Language (ASL) into English via Gemini 1.5’s Video API. Submitted as part of MHacks x Google Hackathon",
+        date: "March 2024",
+        link: "https://github.com/aksheyd/ArtificialNeuralNetwork-NBAFinals"
+    },
+    {
+        name: "Personal Portfolio",
+        bio: "This website",
+        desc: "whole lot",
+        date: "November 2024 - March 2024",
+        link: "https://github.com/aksheyd/aksheyd.github.io"
+    },
+    {
+        name: "NBA Neural Network Finals Predictor",
+        bio: "A Neural Network utilizing Python libraries to predict NBA Champions",
+        desc: "Trained a neural network to predict the chance of NBA teams to make the finals based on championship data from over 25 years of previous seasons utilizing pandas, the NBA API, and tensorflow with scikit-learn",
+        date: "May 2023",
+        link: "https://github.com/aksheyd/MHacks"
+    }
+];
+
+export default projects;
