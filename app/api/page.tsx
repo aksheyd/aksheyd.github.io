7a352b38f611f2854e58f8b20a1bc59b53ae9d1e
Akshey D <aksheyd@umich.edu>
2024-12-04 18:55:27 -0500

    update url

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -3,14 +3,12 @@
 import Divider from '../extras/divider'
 import { RedocStandalone } from 'redoc'
 
-
 export default function API() {
     return (
         <div id="Rest_API" className="min-h-screen p-6 bg-white">
             <Divider />
             <RedocStandalone
-                specUrl="http://localhost:3000/swagger.yml"
-                // specUrl="https://portfolio-eight-lac-63.vercel.app/swagger.yml"
+                specUrl="https://portfolio-eight-lac-63.vercel.app/swagger.yml"
                 options={{
                     theme: { colors: { primary: { main: '#5c67f2' } }, typography: { fontSize: '16px' } },
                 }}
