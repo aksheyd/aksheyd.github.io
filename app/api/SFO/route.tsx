b94a4f0a3ce351e41d67579642b6c8506727b65d
Akshey D <aksheyd@umich.edu>
2024-12-04 19:10:56 -0500

    sfo name change

--- app/api/SFO/route.tsx
+++ app/api/SFO/route.tsx
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {    
-    return NextResponse.json({ Terminals:  [
+    return NextResponse.json({ Terminals: [
         "Harvey Milk Terminal 1",
         "Terminal 2",
         "Terminal 3",
