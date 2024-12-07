bf01094f08b7dcbff3d8cb5b916866529e9947ab
Akshey D <aksheyd@umich.edu>
2024-12-06 19:17:58 -0500

    asset updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import About from "./about/about";
 import Contact from "./contact/contact";
+import Divider from "./extras/divider";
 import Future from "./future/future";
 import Intro from "./intro/intro";
 import Nav from "./nav/nav";
@@ -9,6 +11,7 @@ export default function Page() {
         <div className="overflow-x-hidden"> 
             <Nav/>
             <Intro/>
+            <About/>
             <Work/>
             <Contact/>
             <Future/>
--- app/work/projects.tsx
+++ app/work/projects.tsx
@@ -4,21 +4,40 @@ let projects = [
         bio: "An American Sign Language (ASL) Translator utilizing Google's Gemini API",
         desc: "Collaborated with 2 team members to create a React and Flask web app to allow users to record videos signing and translate American Sign Language (ASL) into English via Gemini 1.5’s Video API. Submitted as part of MHacks x Google Hackathon",
         date: "March 2024",
-        link: "https://github.com/aksheyd/MHacks"
+        link: "https://devpost.com/software/gemini-asl-translator",
+        images: [
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Google_Gemini_logo.svg/2560px-Google_Gemini_logo.svg.png",
+            "https://cdn.worldvectorlogo.com/logos/flask.svg",
+            "https://cdn.worldvectorlogo.com/logos/react-native-1.svg",
+
+        ]
     },
     {
         name: "Personal Portfolio",
         bio: "A website application leveraging modern web technologies",
         desc: "Created using Next.js, React, and Typescript, featuring a REST API, GSAP animations, Tailwind CSS for styling, and Swagger for documentation. Split up into a Github Pages deployment for portfolio and Vercel deployment for cooler features.",
         date: "November 2024 - March 2024",
-        link: "https://github.com/aksheyd/aksheyd.github.io"
+        link: "https://github.com/aksheyd/aksheyd.github.io",
+        images: [
+            "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg",
+            "https://cdn.worldvectorlogo.com/logos/react-2.svg",
+            "https://miro.medium.com/v2/resize:fit:700/1*eZX21KaXanUokLYelxNURg.png",
+            "https://www.cdnlogo.com/logos/t/34/tailwind-css.svg",
+        ]
     },
     {
         name: "NBA Neural Network Finals Predictor",
         bio: "A Neural Network utilizing Python libraries to predict NBA Champions",
         desc: "Trained a neural network to predict the chance of NBA teams to make the finals based on championship data from over 25 years of previous seasons utilizing pandas, the NBA API, and tensorflow with scikit-learn",
         date: "May 2023",
-        link: "https://github.com/aksheyd/ArtificialNeuralNetwork-NBAFinals"
+        link: "https://github.com/aksheyd/ArtificialNeuralNetwork-NBAFinals",
+        images: [
+            "https://cdn.worldvectorlogo.com/logos/python-3.svg",
+            "https://geo-python.github.io/site/2019/_images/pandas_logo.png",
+            "https://seeklogo.com/images/T/tensorflow-logo-AE5100E55E-seeklogo.com.png",
+            "https://scikit-learn.org/stable/_static/scikit-learn-logo-small.png",
+            "https://cdn.worldvectorlogo.com/logos/nba-2.svg",
+        ]
     }
 ];
 
