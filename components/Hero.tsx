81c5d413278cb668650ee88fe7c6a1314405e951
Akshey D <aksheyd@umich.edu>
2025-04-23 21:23:50 -0400

    the vision

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,16 @@
-export default function Hero() {  
+export default function Hero() {
   return (
     <div>
-      <section id="intro" className="comparisonSection z-10 relative min-h-screen w-screen">
-        <div className="dyn_intro flex select-none items-center justify-left w-screen min-h-screen">
-          <div className="comparison text-center">
+      <section id="intro" className="comparisonSection z-10 relative min-h-screen w-screen border-r border-l border-dashed">
+        <div className="flex select-none items-top justify-center w-screen min-h-screen">
+          <div className="comparison text-center mt-10">
             <div className="z-50 text-9xl font-bold tracking-tight text-left">
-              <h1>
-                Akshey
-              </h1>
-              <h1 className="mt-2">
-                Deokule
-              </h1>
+              <h1>Akshey Deokule</h1>
+            </div>
+            <div className="z-50 tracking-tight text-left -my-5">
+              <span>------ I am ----------------------------------- nothing</span>
+              <span className="ml-[3.1rem]">---</span>
+              <span className="ml-[2.2rem]">------ What ----------------- are ------------------- you -----------</span>
             </div>
           </div>
         </div>
