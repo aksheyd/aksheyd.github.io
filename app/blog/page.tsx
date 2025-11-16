88533cbc40079f8c42870d660c395a2d53934a8e
Akshey Deokule (Personal) <aksheyd@umich.edu>
2025-11-15 16:51:38 -0800

    feat: create blog posts route with MDX parsing

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import BlogPage from "@/components/BlogPage";
+
+export default function Page() {
+  return (
+    <div className="overflow-x-hidden bg-background text-foreground">
+      <BlogPage />
+    </div>
+  );
+}
