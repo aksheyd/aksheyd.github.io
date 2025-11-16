0044d6fe35d9d75a2859988d60d943c7e6738440
Akshey Deokule (Personal) <aksheyd@umich.edu>
2025-11-15 16:51:24 -0800

    feat: add MDX support

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
   basePath: "",
   images: {
     unoptimized: true,
   },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 export default nextConfig;
